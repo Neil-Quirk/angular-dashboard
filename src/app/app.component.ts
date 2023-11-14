b18360c2bf9feaf819641e44b4f11669bb637545
Neil Quirk <73691309+Neil-Quirk@users.noreply.github.com>
2023-11-14 21:44:32 +0200

    Finishing Touches
    Updated Bookmarks Links
    Secondary router transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Optional } from '@angular/core';
+import { Component, OnInit, Optional } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {
   trigger,
@@ -10,6 +10,17 @@ import {
   group,
   // ...
 } from '@angular/animations';
+import { Observable, timer } from 'rxjs';
+import { map } from 'rxjs/operators'
+
+const baseStyles = style({
+  // display: 'block',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+})
 
 @Component({
   selector: 'app-root',
@@ -26,14 +37,7 @@ import {
         query(
           ':enter, :leave',
           [
-            style({
-              // display: 'block',
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-            }),
+            baseStyles
           ],
           { optional: true }
         ),
@@ -83,14 +87,7 @@ import {
         query(
           ':enter, :leave',
           [
-            style({
-
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-            }),
+            baseStyles
           ],
           { optional: true }
         ),
@@ -128,12 +125,184 @@ import {
           ),
         ]),
       ]),
+
+
+    //transition to a secondary state (when you bring up a manage or add window)
+      transition('* => secondary', [
+        style({
+          position: 'relative',
+
+        }),
+        query(
+          ':enter, :leave',
+          [
+            baseStyles
+          ],
+          { optional: true }
+        ),
+        group([
+          query(
+            ':leave',
+            [
+              animate(
+                '200ms ease-in',
+                style({
+                  opacity: 0,
+                  transform:'scale(0.8)'
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+
+          query(
+            ':enter',
+            [
+              style({
+                transform:'scale(1.2)',
+                opacity: 0
+              }),
+              animate(
+                '200ms 100ms ease-out',
+                style({
+                  opacity: 1,
+                  transform: 'scale(1.0)'
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+        ]),
+      ]),
+      //END OF TRANSITION TO SECONDARY STATE
+
+      //BEGINNING OF TRANSITION FROM SECONDARY STATE
+
+      transition('secondary => *', [
+        style({
+          position: 'relative',
+
+        }),
+        query(
+          ':enter, :leave',
+          [
+            baseStyles
+          ],
+          { optional: true }
+        ),
+        group([
+          query(
+            ':leave',
+            [
+              animate(
+                '200ms ease-in',
+                style({
+                  opacity: 0,
+                  transform:'scale(1.25)'
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+
+          query(
+            ':enter',
+            [
+              style({
+                transform:'scale(0.8)',
+                opacity: 0
+              }),
+              animate(
+                '200ms 100ms ease-out',
+                style({
+                  opacity: 1,
+                  transform: 'scale(1.0)'
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+        ]),
+      ]),
+
+      //END OF TRANSITION FROM SECONDARY STATE
+
     ]),
+
+    // This is the trigger to fade out the background change
+    trigger('bgAnim', [
+      transition(':leave', [
+        animate(1200, style({
+          opacity: 0
+        }))
+      ])
+    ]),
+    //This trigger caused the background change button to fade in and out when clicked
+    trigger('fadeAnim', [
+      transition(':enter', [
+        style({ opacity: 0}),
+          animate(250, style({
+            opacity: 1
+          }))
+      ]),
+      transition(':leave', [
+        animate(250, style({
+          opacity: 0
+        }))
+      ])
+    ])
   ],
 })
-export class AppComponent {
-  bg: string = ''
+export class AppComponent implements OnInit{
+  backgrounds: string[] = ['https://images.unsplash.com/photo-1699865701680-ef9214d90fbf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D']
+
+  loadingBGImage: boolean | undefined
+
+  dateTime: Observable<Date> | undefined
+  
+  ngOnInit(): void {
+    this.dateTime = timer(0, 1000).pipe(
+      map(() => {
+        return new Date()
+      })
+    )
+  }
+
+
   prepareRoute(outlet: RouterOutlet) {
-    if (outlet.isActivated) return outlet.activatedRouteData['tab'];
+    if (outlet.isActivated) { 
+      const tab = outlet.activatedRouteData['tab']
+      if(!tab) return 'secondary'
+      return tab
+    };
+  }
+  async changeBGImage():Promise<void> {
+    this.loadingBGImage = true
+    const response = await fetch('https://source.unsplash.com/random/ 1920x1080', {
+      method:'HEAD'
+    })
+
+    const alreadyGot = this.backgrounds.includes(response.url)
+    if(alreadyGot){
+      return this.changeBGImage()
+    }
+
+    this.backgrounds.push(response.url)
+
+    // const newBG = response.url;
+
+    // if(newBG === this.backgrounds) 
+    // return this.changeBGImage()
+
+    // this.backgrounds = newBG
+  }
+  onBGImageLoad(imgEvent: Event){
+ //bg image has loaded so now remove the old BG image from backgrounds array
+    const imgElement = imgEvent.target as HTMLImageElement
+    const src = imgElement.src
+    this.backgrounds = this.backgrounds.filter(b => b === src)
+
+
+    this.loadingBGImage = false
   }
 }
